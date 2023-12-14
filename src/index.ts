2d8d4430670a860b1e662570db7d9a2b00b83b7d
Lukáš Hornych <lukashornych@proton.me>
2023-12-14 22:21:09 +0100

    feat: evitaQL grammar (WIP)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent, syntaxTree} from "@codemirror/language"
+import { parser } from './syntax.grammar'
+import {
+    LRLanguage,
+    LanguageSupport,
+    indentNodeProp,
+    foldNodeProp,
+    foldInside,
+    delimitedIndent,
+    syntaxTree
+} from '@codemirror/language'
 import { completeFromList, CompletionContext, CompletionResult } from '@codemirror/autocomplete'
-import {styleTags, tags as t} from "@lezer/highlight"
+import { styleTags, tags as t } from '@lezer/highlight'
 import { Diagnostic, linter } from '@codemirror/lint'
 
 /*
@@ -18,213 +26,249 @@ import { Diagnostic, linter } from '@codemirror/lint'
  */
 
 export const evitaQLLanguage = LRLanguage.define({
-  parser: parser.configure({
-    props: [
-      indentNodeProp.add({
-        Query: delimitedIndent({closing: ")", align: true}),
-        HeadConstraint: delimitedIndent({closing: ")", align: true}),
-        FilterConstraint: delimitedIndent({closing: ")", align: true}),
-        Range: delimitedIndent({closing: "]", align: true}),
-      }),
-      // todo this is not working
-      // foldNodeProp.add({
-      //   Query: foldInside,
-      //   HeadConstraint: foldInside,
-      //   FilterConstraint: foldInside,
-      // }),
-      styleTags({
-        String: t.string,
-        Int: t.integer,
-        Float: t.float,
-        Boolean: t.bool,
-        Date: t.literal,
-        Time: t.literal,
-        DateTime: t.literal,
-        OffsetDateTime: t.literal,
-        Uuid: t.literal,
-        Enum: t.constant(t.variableName),
-        Query: t.function(t.variableName),
-        HeadConstraint: t.function(t.variableName),
-        FilterConstraint: t.function(t.variableName),
-        Comment: t.lineComment,
-        ",": t.separator,
-        "( )": t.paren,
-        "[ ]": t.squareBracket,
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {line: "//"}
-  }
+    parser: parser.configure({
+        props: [
+            indentNodeProp.add({
+                Query: delimitedIndent({ closing: ')', align: true }),
+                HeadConstraint: delimitedIndent({ closing: ')', align: true }),
+                FilterConstraint: delimitedIndent({ closing: ')', align: true }),
+                Range: delimitedIndent({ closing: ']', align: true })
+            }),
+            // todo this is not working
+            // foldNodeProp.add({
+            //   Query: foldInside,
+            //   HeadConstraint: foldInside,
+            //   FilterConstraint: foldInside,
+            // }),
+            styleTags({
+                String: t.string,
+                Int: t.integer,
+                Float: t.float,
+                Boolean: t.bool,
+                Date: t.literal,
+                Time: t.literal,
+                DateTime: t.literal,
+                OffsetDateTime: t.literal,
+                Uuid: t.literal,
+                Enum: t.constant(t.variableName),
+                Query: t.function(t.variableName),
+                HeadConstraint: t.function(t.variableName),
+                FilterConstraint: t.function(t.variableName),
+                Comment: t.lineComment,
+                ',': t.separator,
+                '( )': t.paren,
+                '[ ]': t.squareBracket
+            })
+        ]
+    }),
+    languageData: {
+        commentTokens: { line: '//' }
+    }
 })
 
 // todo
-// function getEvitaQLCompletions(context: CompletionContext): CompletionResult | null {
-  // let word = context.matchBefore(/\(/)
-  // if (word == null || (word.from == word.to && !context.explicit)) {
-  //   return null
-  // }
-  // console.log(word)
-  // if (word.text === "filterBy") {
-  //   return {
-  //     from: word.from,
-  //     options:  [
-  //       { type: "function", label: "and" },
-  //       { type: "function", label: "attributeEquals" }
-  //     ]
-  //   }
-  // }
-  // return {
-  //   from: word.from,
-  //   options: []
-  // }
+function getEvitaQLCompletions(context: CompletionContext): CompletionResult | null {
+    // let word = context.matchBefore(/\(/)
+    // if (word == null || (word.from == word.to && !context.explicit)) {
+    //   return null
+    // }
+    // console.log(word)
+    // if (word.text === "filterBy") {
+    //   return {
+    //     from: word.from,
+    //     options:  [
+    //       { type: "function", label: "and" },
+    //       { type: "function", label: "attributeEquals" }
+    //     ]
+    //   }
+    // }
+    // return {
+    //   from: word.from,
+    //   options: []
+    // }
 
 
-  // const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
-  // const nodeName: string = nodeBefore.name
-  //
-  // console.log(nodeBefore)
-  // console.log(nodeName)
-  // console.log(context.pos)
-  //
-  // if (nodeName === "Query") {
-  //   const textBefore = context.state.sliceDoc(nodeBefore.from, context.pos)
-  //   const tagBefore = /query\($/.exec(textBefore)
-  //   if (!tagBefore && !context.explicit) return null
-  //   return {
-  //     from: tagBefore ? nodeBefore.from + tagBefore.index : context.pos,
-  //     options: [
-  //       { label: "filterBy", type: "function" }
-  //     ],
-  //     validFor: /^(query\()?$/
-  //   }
-  // }
-  //
-  // return null
-// }
+    const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
+    const parentNode = nodeBefore.parent
+    if (parentNode == null) {
+        return null
+    }
+
+    if (parentNode.name === 'Request') {
+        return {
+            from: context.pos,
+            options: [
+                { label: 'query', type: 'function' }
+            ]
+        }
+    }
+    if (parentNode.name === 'Query') {
+        return {
+            from: context.pos,
+            options: [
+                { label: 'collection', type: 'function' },
+                { label: 'filterBy', type: 'function' },
+                { label: 'orderBy', type: 'function' },
+                { label: 'require', type: 'function' },
+            ]
+        }
+    }
+    if (parentNode.name === 'HeadConstraint') {
+        return {
+            from: context.pos,
+            options: [
+                { label: 'collection', type: 'function' }
+            ]
+        }
+    }
+    if (parentNode.name === 'FilterConstraint') {
+        return {
+            from: context.pos,
+            options: [
+                { label: 'and', type: 'function' },
+                { label: 'attributeEquals', type: 'function' },
+            ]
+        }
+    }
+
+    // if (nodeName === 'Query') {
+    //     const textBefore = context.state.sliceDoc(nodeBefore.from, context.pos)
+    //     const tagBefore = /query\($/.exec(textBefore)
+    //     if (!tagBefore && !context.explicit) return null
+    //     return {
+    //         from: tagBefore ? nodeBefore.from + tagBefore.index : context.pos,
+    //         options: [
+    //             { label: 'filterBy', type: 'function' }
+    //         ],
+    //         validFor: /^(query\()?$/
+    //     }
+    // }
+
+    return null
+}
 
 export const evitaQLCompletion = evitaQLLanguage.data.of({
-  autocomplete: completeFromList([
-    "query",
+    autocomplete: completeFromList([
+        'query',
 
-    "collection",
+        'collection',
 
-    "filterBy",
-    "filterGroupBy",
-    "and",
-    "or",
-    "not",
-    "userFilter",
-    "attributeEquals",
-    "attributeGreaterThan",
-    "attributeGreaterThanEquals",
-    "attributeLessThan",
-    "attributeLessThanEquals",
-    "attributeBetween",
-    "attributeInSet",
-    "attributeContains",
-    "attributeStartsWith",
-    "attributeEndsWith",
-    "attributeEqualsTrue",
-    "attributeEqualsFalse",
-    "attributeIs",
-    "attributeIsNull",
-    "attributeIsNotNull",
-    "attributeInRange",
-    "attributeInRangeNow",
-    "entityPrimaryKeyInSet",
-    "entityLocaleEquals",
-    "priceInCurrency",
-    "priceInPriceLists",
-    "priceValidInNow",
-    "priceValidIn",
-    "priceBetween",
-    "facetHaving",
-    "referenceHaving",
-    "hierarchyWithin",
-    "hierarchyWithinSelf",
-    "hierarchyWithinRoot",
-    "hierarchyWithinRootSelf",
-    "directRelation",
-    "having",
-    "excludingRoot",
-    "excluding",
-    "entityHaving",
+        'filterBy',
+        'filterGroupBy',
+        'and',
+        'or',
+        'not',
+        'userFilter',
+        'attributeEquals',
+        'attributeGreaterThan',
+        'attributeGreaterThanEquals',
+        'attributeLessThan',
+        'attributeLessThanEquals',
+        'attributeBetween',
+        'attributeInSet',
+        'attributeContains',
+        'attributeStartsWith',
+        'attributeEndsWith',
+        'attributeEqualsTrue',
+        'attributeEqualsFalse',
+        'attributeIs',
+        'attributeIsNull',
+        'attributeIsNotNull',
+        'attributeInRange',
+        'attributeInRangeNow',
+        'entityPrimaryKeyInSet',
+        'entityLocaleEquals',
+        'priceInCurrency',
+        'priceInPriceLists',
+        'priceValidInNow',
+        'priceValidIn',
+        'priceBetween',
+        'facetHaving',
+        'referenceHaving',
+        'hierarchyWithin',
+        'hierarchyWithinSelf',
+        'hierarchyWithinRoot',
+        'hierarchyWithinRootSelf',
+        'directRelation',
+        'having',
+        'excludingRoot',
+        'excluding',
+        'entityHaving',
 
-    "orderBy",
-    "orderGroupBy",
-    "attributeNatural",
-    "attributeSetExact",
-    "attributeSetInFilter",
-    "priceNatural",
-    "random",
-    "referenceProperty",
-    "entityPrimaryKeyNatural",
-    "entityPrimaryKeyExact",
-    "entityPrimaryKeyInFilter",
-    "entityProperty",
-    "entityGroupProperty",
+        'orderBy',
+        'orderGroupBy',
+        'attributeNatural',
+        'attributeSetExact',
+        'attributeSetInFilter',
+        'priceNatural',
+        'random',
+        'referenceProperty',
+        'entityPrimaryKeyNatural',
+        'entityPrimaryKeyExact',
+        'entityPrimaryKeyInFilter',
+        'entityProperty',
+        'entityGroupProperty',
 
-    "require",
-    "page",
-    "strip",
-    "entityFetch",
-    "entityGroupFetch",
-    "attributeContent",
-    "attributeContentAll",
-    "priceContent",
-    "priceContentAll",
-    "priceContentRespectingFilter",
-    "associatedDataContent",
-    "associatedDataContentAll",
-    "referenceContentAll",
-    "referenceContent",
-    "referenceContentAllWithAttributes",
-    "referenceContentWithAttributes",
-    "hierarchyContent",
-    "priceType",
-    "dataInLocalesAll",
-    "dataInLocales",
-    "facetSummary",
-    "facetSummaryOfReference",
-    "facetGroupsConjunction",
-    "facetGroupsDisjunction",
-    "facetGroupsNegation",
-    "attributeHistogram",
-    "priceHistogram",
-    "distance",
-    "level",
-    "node",
-    "stopAt",
-    "statistics",
-    "fromRoot",
-    "children",
-    "siblings",
-    "parents",
-    "hierarchyOfSelf",
-    "hierarchyOfReference",
-    "queryTelemetry"
-  ].map(constraint => ({label: constraint, type: "function"})))
+        'require',
+        'page',
+        'strip',
+        'entityFetch',
+        'entityGroupFetch',
+        'attributeContent',
+        'attributeContentAll',
+        'priceContent',
+        'priceContentAll',
+        'priceContentRespectingFilter',
+        'associatedDataContent',
+        'associatedDataContentAll',
+        'referenceContentAll',
+        'referenceContent',
+        'referenceContentAllWithAttributes',
+        'referenceContentWithAttributes',
+        'hierarchyContent',
+        'priceType',
+        'dataInLocalesAll',
+        'dataInLocales',
+        'facetSummary',
+        'facetSummaryOfReference',
+        'facetGroupsConjunction',
+        'facetGroupsDisjunction',
+        'facetGroupsNegation',
+        'attributeHistogram',
+        'priceHistogram',
+        'distance',
+        'level',
+        'node',
+        'stopAt',
+        'statistics',
+        'fromRoot',
+        'children',
+        'siblings',
+        'parents',
+        'hierarchyOfSelf',
+        'hierarchyOfReference',
+        'queryTelemetry'
+    ].map(constraint => ({ label: constraint, type: 'function' })))
 })
 
 export const evitaQLLinter = linter(view => {
-  const diagnostics: Diagnostic[] = [];
+    const diagnostics: Diagnostic[] = []
 
-  syntaxTree(view.state).cursor().iterate(node => {
-    if (node.type.isError) {
-      console.log(node)
-      diagnostics.push({
-        from: node.from,
-        to: node.to,
-        severity: "error",
-        message: "Syntax error.",
-      });
-    }
-  });
+    syntaxTree(view.state).cursor().iterate(node => {
+        if (node.type.isError) {
+            console.log(node)
+            diagnostics.push({
+                from: node.from,
+                to: node.to,
+                severity: 'error',
+                message: 'Syntax error.'
+            })
+        }
+    })
 
-  return diagnostics;
+    return diagnostics
 })
 
 export function evitaQL() {
-  return new LanguageSupport(evitaQLLanguage, [evitaQLCompletion, evitaQLLinter])
+    return new LanguageSupport(evitaQLLanguage, [evitaQLCompletion, evitaQLLinter])
 }

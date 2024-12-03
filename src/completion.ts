38da8bf43047a377a27d37ca94ae7d0ecb319926
Lukáš Hornych <lukashornych@proton.me>
2024-12-03 09:52:38 +0100

    feat(constraints): inScope constraint support

--- src/completion.ts
+++ src/completion.ts
@@ -54,73 +54,6 @@ export function evitaQLCompletion(lang: LRLanguage, config: EvitaQLConfig): Exte
     })
 }
 
-function getEvitaQLCompletions(context: CompletionContext): CompletionResult | null {
-    const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
-
-    const result: CompletionResult | null = (() => {
-        let textBefore = context.state.sliceDoc(nodeBefore.from, context.pos)
-        let tagBefore = /[a-zA-Z]+/.exec(textBefore)
-        if (!tagBefore && !context.explicit) {
-            return null
-        }
-
-        if (nodeBefore.name === 'Request') {
-            return getRequestCompletions(context, nodeBefore, tagBefore as RegExpExecArray)
-        } else if (nodeBefore.name === 'QueryBody') {
-            return getQueryCompletions(context, nodeBefore, tagBefore as RegExpExecArray)
-        } else if (nodeBefore.name === 'ConstraintBody') {
-            return getConstraintCompletions(context, nodeBefore, tagBefore as RegExpExecArray)
-        }
-
-        let parentNode = nodeBefore.parent
-        if (parentNode == null) {
-            return null
-        }
-        textBefore = context.state.sliceDoc(nodeBefore.from, context.pos)
-        tagBefore = /[a-zA-Z]+/.exec(textBefore)
-        if (!tagBefore && !context.explicit) {
-            return null
-        }
-        if (parentNode.name === 'Request') {
-            return getRequestCompletions(context, parentNode, tagBefore as RegExpExecArray)
-        } else if (parentNode.name === 'QueryBody') {
-            return getQueryCompletions(context, parentNode, tagBefore as RegExpExecArray)
-        } else if (parentNode.name === 'ConstraintBody') {
-            return getConstraintCompletions(context, parentNode, tagBefore as RegExpExecArray)
-        }
-
-        return null
-    })()
-
-    console.log(result)
-    return result
-}
-
-function getRequestCompletions(context: CompletionContext, node: SyntaxNode, tagBefore: RegExpExecArray): CompletionResult {
-    return {
-        from: tagBefore ? node.from + tagBefore.index : context.pos,
-        options: [
-            createCompletion('query', '`query` is the root construct for querying data.'),
-            createCompletion('query2', '`query` is the root construct for querying data.')
-        ],
-        validFor: /^(\w+)?$/
-    }
-}
-function getQueryCompletions(context: CompletionContext, node: SyntaxNode, tagBefore: RegExpExecArray): CompletionResult {
-    return {
-        from: tagBefore ? node.from + tagBefore.index : context.pos,
-        options: ['collection', 'filterBy', 'orderBy', 'require'].map(it => createCompletion(it)),
-        validFor: /^(\w+)?$/
-    }
-}
-function getConstraintCompletions(context: CompletionContext, node: SyntaxNode, tagBefore: RegExpExecArray): CompletionResult {
-    return {
-        from: tagBefore ? node.from + tagBefore.index : context.pos,
-        options: Object.keys(constraints).map(it => createCompletion(it)),
-        validFor: /^(\w+)?$/
-    }
-}
-
 function createCompletion(label: string, info?: string): Completion {
     const constraintDefinition: any = constraints[label]
     return {
@@ -136,3 +69,71 @@ function createCompletion(label: string, info?: string): Completion {
         }
     }
 }
+
+// TODO lho possible custom solution
+// function getEvitaQLCompletions(context: CompletionContext): CompletionResult | null {
+//     const nodeBefore = syntaxTree(context.state).resolveInner(context.pos, -1)
+//
+//     const result: CompletionResult | null = (() => {
+//         let textBefore = context.state.sliceDoc(nodeBefore.from, context.pos)
+//         let tagBefore = /[a-zA-Z]+/.exec(textBefore)
+//         if (!tagBefore && !context.explicit) {
+//             return null
+//         }
+//
+//         if (nodeBefore.name === 'Request') {
+//             return getRequestCompletions(context, nodeBefore, tagBefore as RegExpExecArray)
+//         } else if (nodeBefore.name === 'QueryBody') {
+//             return getQueryCompletions(context, nodeBefore, tagBefore as RegExpExecArray)
+//         } else if (nodeBefore.name === 'ConstraintBody') {
+//             return getConstraintCompletions(context, nodeBefore, tagBefore as RegExpExecArray)
+//         }
+//
+//         let parentNode = nodeBefore.parent
+//         if (parentNode == null) {
+//             return null
+//         }
+//         textBefore = context.state.sliceDoc(nodeBefore.from, context.pos)
+//         tagBefore = /[a-zA-Z]+/.exec(textBefore)
+//         if (!tagBefore && !context.explicit) {
+//             return null
+//         }
+//         if (parentNode.name === 'Request') {
+//             return getRequestCompletions(context, parentNode, tagBefore as RegExpExecArray)
+//         } else if (parentNode.name === 'QueryBody') {
+//             return getQueryCompletions(context, parentNode, tagBefore as RegExpExecArray)
+//         } else if (parentNode.name === 'ConstraintBody') {
+//             return getConstraintCompletions(context, parentNode, tagBefore as RegExpExecArray)
+//         }
+//
+//         return null
+//     })()
+//
+//     console.log(result)
+//     return result
+// }
+
+// function getRequestCompletions(context: CompletionContext, node: SyntaxNode, tagBefore: RegExpExecArray): CompletionResult {
+//     return {
+//         from: tagBefore ? node.from + tagBefore.index : context.pos,
+//         options: [
+//             createCompletion('query', '`query` is the root construct for querying data.'),
+//             createCompletion('query2', '`query` is the root construct for querying data.')
+//         ],
+//         validFor: /^(\w+)?$/
+//     }
+// }
+// function getQueryCompletions(context: CompletionContext, node: SyntaxNode, tagBefore: RegExpExecArray): CompletionResult {
+//     return {
+//         from: tagBefore ? node.from + tagBefore.index : context.pos,
+//         options: ['collection', 'filterBy', 'orderBy', 'require'].map(it => createCompletion(it)),
+//         validFor: /^(\w+)?$/
+//     }
+// }
+// function getConstraintCompletions(context: CompletionContext, node: SyntaxNode, tagBefore: RegExpExecArray): CompletionResult {
+//     return {
+//         from: tagBefore ? node.from + tagBefore.index : context.pos,
+//         options: Object.keys(constraints).map(it => createCompletion(it)),
+//         validFor: /^(\w+)?$/
+//     }
+// }
